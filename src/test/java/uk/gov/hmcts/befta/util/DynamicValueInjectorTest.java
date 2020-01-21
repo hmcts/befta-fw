15168d7c0db01fc8ba44ea9ea58ab9fc63ad7670
RBaker <rebecca.baker@hmcts.net>
2020-01-21 16:39:44 +0000

    RDM-7245 refactored tests to cater for all complex scenarios

--- src/test/java/uk/gov/hmcts/befta/util/DynamicValueInjectorTest.java
+++ src/test/java/uk/gov/hmcts/befta/util/DynamicValueInjectorTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.befta.util;
 
+import org.apache.logging.log4j.util.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -55,14 +56,14 @@ public class DynamicValueInjectorTest {
     }
 
     @Test
-    public void shouldInjectAllEnvironmentVariables() {
+    public void shouldInjectAllEnvironmentAndFormulaVariables() {
 
         scenarioContext = new BackEndFunctionalTestScenarioContextForTest();
 
-        scenarioContext.initializeTestDataFor("Simple-Test-Data-With-All-Possible-Dynamic-Data-Values");
+        scenarioContext.initializeTestDataFor("Complex-Test-Data-With-All-Possible-Dynamic-Values");
 
         BackEndFunctionalTestScenarioContext subcontext = new BackEndFunctionalTestScenarioContextForTest();
-        subcontext.initializeTestDataFor("Token_Creation_Call_Data");
+        subcontext.initializeTestDataFor("Token_Creation_Call_For_Complex_Data");
         subcontext.getTestData().setActualResponse(subcontext.getTestData().getExpectedResponse());
 
         scenarioContext.setTheInvokingUser(scenarioContext.getTestData().getInvokingUser());
@@ -81,6 +82,18 @@ public class DynamicValueInjectorTest {
         Assert.assertEquals("http://http://localhost:5000/documents/binary", testData.getRequest().getPathVariables().get("token"));
         Assert.assertEquals("http://localhost:5000", testData.getRequest().getPathVariables().get("token_2"));
         Assert.assertEquals("http://http://localhost:5000/documents/binary", testData.getRequest().getBody().get("event_token"));
+
+        Assert.assertEquals(null, testData.getRequest().getBody().get("nullValueField"));
+        Assert.assertEquals(Strings.EMPTY, testData.getRequest().getBody().get("emptyString"));
+        Assert.assertEquals(4.6, testData.getRequest().getBody().get("onlyStaticNumber"));
+        Assert.assertEquals("string without any dynamic part", testData.getRequest().getBody().get("onlyStaticString"));
+        Assert.assertEquals("token value at index 2", testData.getRequest().getBody().get("onlyFormulaOnly"));
+        Assert.assertEquals("http://localhost:5000", testData.getRequest().getBody().get("oneEnvironmentVariableOnly"));
+        Assert.assertEquals("http://localhost:5000Pa55word11http://localhost:4451", testData.getRequest().getBody().get("threeEnvironmentVariablesOnly"));
+        Assert.assertEquals("token value at index 2token value at index 2", testData.getRequest().getBody().get("twoFormulasOnly"));
+        Assert.assertEquals("token value at index 2http://localhost:5000abc123http://localhost:4451", testData.getRequest().getBody().get("complicatedNestedValue_1"));
+        Assert.assertEquals("abctoken value at index 2.=.http://localhost:5000token value at index 2abc123http://localhost:4451", testData.getRequest().getBody().get("complicatedNestedValue_2"));
+
     }
 
     class BackEndFunctionalTestScenarioContextForTest extends BackEndFunctionalTestScenarioContext {
