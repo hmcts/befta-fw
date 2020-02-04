baa684283c95e4e69279206ab25ba4b2d9d52bdd
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-02-04 20:56:12 +0000

    RDM-7283: Introduce Validation Literals more Specific than just '[[ANY]]'

--- src/main/java/uk/gov/hmcts/befta/util/ExpectedValuePlaceholder.java
+++ src/main/java/uk/gov/hmcts/befta/util/ExpectedValuePlaceholder.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.befta.util;
 
-import java.sql.Timestamp;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public enum ExpectedValuePlaceholder {
 
@@ -11,12 +9,12 @@ public enum ExpectedValuePlaceholder {
     ANYTHING_IF_EXISTS(true),
 
     ANY_NULLABLE(true),
-    ANY_STRING_NULLABLE(true, String.class),
-    ANY_NUMBER_NULLABLE(true, Number.class),
-    ANY_INTEGER_NULLABLE(true, Integer.class),
-    ANY_FLOATING_NULLABLE(true, Float.class),
-    ANY_DATE_NULLABLE(true,  Date.class),
-    ANY_TIMESTAMP_NULLABLE(true, Timestamp.class),
+    ANY_STRING_NULLABLE(true),
+    ANY_NUMBER_NULLABLE(true),
+    ANY_INTEGER_NULLABLE(true),
+    ANY_FLOATING_NULLABLE(true),
+    ANY_DATE_NULLABLE(true),
+    ANY_TIMESTAMP_NULLABLE(true),
     ANY_OBJECT_NULLABLE(true),
 
     ANY_NOT_NULLABLE(false),
@@ -24,21 +22,15 @@ public enum ExpectedValuePlaceholder {
     ANY_NUMBER_NOT_NULLABLE(false),
     ANY_INTEGER_NOT_NULLABLE(false),
     ANY_FLOATING_NOT_NULLABLE(false),
-    ANY_DATE_NOT_NULLABLE(false, Date.class),
-    ANY_TIMESTAMP_NOT_NULLABLE(false, Timestamp.class),
+    ANY_DATE_NOT_NULLABLE(false),
+    ANY_TIMESTAMP_NOT_NULLABLE(false),
     ANY_OBJECT_NOT_NULLABLE(false);
 
 
     private boolean nullable;
-    private Class clazz;
 
     ExpectedValuePlaceholder(boolean nullable) {
-        this(nullable, Object.class);
-    }
-
-    ExpectedValuePlaceholder(boolean nullable, Class clazz) {
         this.nullable = nullable;
-        this.clazz = clazz;
     }
 
     public boolean isNullable() {
@@ -54,20 +46,35 @@ public enum ExpectedValuePlaceholder {
         boolean valid = true;
         if (actualObject != null) {
             try {
-                if (this.clazz == String.class) {
-                    valid &= actualObject instanceof String;
-                } else if (this.clazz == Number.class) {
-                    NumberFormat.getInstance().parse((String) actualObject);
-                } else if (this.clazz == Integer.class) {
-                    Integer.parseInt((String) actualObject);
-                } else if (this.clazz == Float.class) {
-                    Float.parseFloat((String) actualObject);
-                } else if (this.clazz == Date.class) {
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                    simpleDateFormat.parse((String) actualObject);
-                } else if (this.clazz == Timestamp.class) {
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-                    simpleDateFormat.parse((String) actualObject);
+                switch (this) {
+                    case ANY_STRING_NULLABLE:
+                    case ANY_STRING_NOT_NULLABLE:
+                        valid &= actualObject instanceof String;
+                        break;
+                    case ANY_INTEGER_NOT_NULLABLE:
+                    case ANY_INTEGER_NULLABLE:
+                        Integer.parseInt(actualObject.toString());
+                        break;
+                    case ANY_NUMBER_NOT_NULLABLE:
+                    case ANY_NUMBER_NULLABLE:
+                        NumberFormat.getInstance().parse(actualObject.toString());
+                        break;
+                    case ANY_FLOATING_NOT_NULLABLE:
+                    case ANY_FLOATING_NULLABLE:
+                        Float.parseFloat(actualObject.toString());
+                        break;
+                    case ANY_DATE_NOT_NULLABLE:
+                    case ANY_DATE_NULLABLE: {
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        simpleDateFormat.parse(actualObject.toString());
+                        break;
+                    }
+                    case ANY_TIMESTAMP_NOT_NULLABLE:
+                    case ANY_TIMESTAMP_NULLABLE: {
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+                        simpleDateFormat.parse(actualObject.toString());
+                        break;
+                    }
                 }
             } catch (Exception e) {
                 valid &= false;
