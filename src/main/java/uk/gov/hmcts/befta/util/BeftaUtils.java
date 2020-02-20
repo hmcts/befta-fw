de88fafd2e5360b85450ce0cd88990f12d8bb298
mutlu <mutlu.sancaktutar@hmcts.net>
2020-02-20 01:13:44 +0000

    fix temp file creation

--- src/main/java/uk/gov/hmcts/befta/util/BeftaUtils.java
+++ src/main/java/uk/gov/hmcts/befta/util/BeftaUtils.java
@@ -11,11 +11,12 @@ public class BeftaUtils {
     public static File getClassPathResourceIntoTemporaryFile(String resourcePath) {
         try {
             int nameStartsAt = resourcePath.lastIndexOf("/");
-            String simpleName = resourcePath.substring(nameStartsAt < 0 ? 0 : nameStartsAt);
+            String simpleName = resourcePath.substring(nameStartsAt + 1);
             InputStream stream = BeftaUtils.class.getClassLoader().getResource(resourcePath).openStream();
             byte[] buffer = new byte[stream.available()];
             stream.read(buffer);
             File tempFile = new File("_temp_" + System.currentTimeMillis() + "_" + simpleName);
+            tempFile.createNewFile();
             OutputStream outStream = new FileOutputStream(tempFile);
             outStream.write(buffer);
             outStream.close();
