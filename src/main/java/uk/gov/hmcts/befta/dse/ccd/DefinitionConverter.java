2b24a0e712614abbb0efd62b73acaf4819af1f86
mutlu <mutlu.sancaktutar@hmcts.net>
2020-04-09 14:54:17 +0100

    introduce a dummy draft of converter main class

---
+++ src/main/java/uk/gov/hmcts/befta/dse/ccd/DefinitionConverter.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.befta.dse.ccd;
+
+//AC-1
+public class DefinitionConverter {
+    // java DefinitionConverter to-json xyz.xls
+    // java DefinitionConverter to-excel xyz.json
+    public static void main(String[] args) {
+
+    }
+
+}
