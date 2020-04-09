7de82e877c2dcc86630e496ba68484c99d4a0fb2
mutlu <mutlu.sancaktutar@hmcts.net>
2020-04-09 14:55:16 +0100

    introduce dummy draft DSE utility for ccd FTA

---
+++ src/main/java/uk/gov/hmcts/befta/dse/ccd/CcdBeftaUtils.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.befta.dse.ccd;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.File;
+
+public class CcdBeftaUtils {
+
+    public static File convertJsonFileToExcel(String jsonFileName) {
+        return null;
+    }
+
+    public static File convertJsonNodeToExcel(JsonNode jsonNode) {
+        return null;
+    }
+
+    public static File convertExcelFileToJson(String excelFileName) {
+        return null;
+    }
+
+    public static File getExcelFileForDefinition(String jurisdictionId) {
+        // TODO: Use a json store to load the definition with its jurisdiction id
+        return null;
+    }
+}
