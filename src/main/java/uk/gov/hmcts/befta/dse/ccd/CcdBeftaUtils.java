3110d65b6d57a54f83cc91e0ca06e2dbfa73319b
veeresha-hmcts <>
2020-05-11 19:40:51 +0100

    ccdbeftautils file added

---
+++ src/main/java/uk/gov/hmcts/befta/dse/ccd/CcdBeftaUtils.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.befta.dse.ccd;
+
+import uk.gov.hmcts.befta.dse.ccd.definition.converter.ExcelTransformer;
+import uk.gov.hmcts.befta.dse.ccd.definition.converter.JsonTransformer;
+
+import java.io.File;
+
+public class CcdBeftaUtils {
+
+    public static File convertJsonDefinitionToExcel(String jsonFolderPath) {
+        String path = new JsonTransformer(jsonFolderPath).transformToExcel();
+        return new File(path);
+    }
+
+    public static File convertJsonDefinitionToExcel(File jsonFolder) {
+        String path = new JsonTransformer(jsonFolder.getPath()).transformToExcel();
+        return new File(path);
+    }
+
+    public static File convertExcelFileToJson(String excelFilePath) {
+        String path = new ExcelTransformer(excelFilePath).transformToJson();
+        return new File(path);
+    }
+
+    public static File convertExcelFileToJson(File excelFile) {
+        String path = new ExcelTransformer(excelFile.getPath()).transformToJson();
+        return new File(path);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
