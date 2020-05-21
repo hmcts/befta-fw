5ea38e36d28a7bc073513cc2a755a3c8a1c6dfe6
mutlu <mutlu.sancaktutar@hmcts.net>
2020-05-21 12:07:34 +0100

    use def serial version value

--- src/main/java/uk/gov/hmcts/befta/exception/DefinitionTransformerException.java
+++ src/main/java/uk/gov/hmcts/befta/exception/DefinitionTransformerException.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.befta.exception;
 
 public class DefinitionTransformerException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public DefinitionTransformerException(final String message) {
         super(message);
     }
