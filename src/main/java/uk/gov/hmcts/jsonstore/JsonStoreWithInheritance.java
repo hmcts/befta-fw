c49c32d032a4f65e41cf882bab4db648db9752ae
mutlu <mutlu.sancaktutar@hmcts.net>
2020-06-15 04:25:44 +0100

    handle missed condition

--- src/main/java/uk/gov/hmcts/jsonstore/JsonStoreWithInheritance.java
+++ src/main/java/uk/gov/hmcts/jsonstore/JsonStoreWithInheritance.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.BooleanNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.Sets;
 
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,7 +15,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
-import com.google.common.collect.Sets;
 import uk.gov.hmcts.befta.exception.InvalidTestDataException;
 import uk.gov.hmcts.befta.util.ReflectionUtils;
 
@@ -177,10 +177,12 @@ public abstract class JsonStoreWithInheritance {
     }
 
     private void overlayFieldWith(JsonNode overlaidField, JsonNode overlayingField) {
-        if (!overlayingField.isArray()) {
-            Iterator<String> overlayingFields = overlayingField.fieldNames();
-            while (overlayingFields.hasNext()) {
-                String overlayingSubFieldName = overlayingFields.next();
+        if (overlayingField.isArray()) {
+                ((ArrayNode) overlaidField).addAll((ArrayNode) overlayingField);
+        } else {
+            Iterator<String> overlayingSubfields = overlayingField.fieldNames();
+            while (overlayingSubfields.hasNext()) {
+                String overlayingSubFieldName = overlayingSubfields.next();
                 if (!isInheritanceMechanismField(overlayingSubFieldName)) {
                     JsonNode overlayingSubField = overlayingField.get(overlayingSubFieldName);
                     JsonNode overlaidSubField = overlaidField.get(overlayingSubFieldName);
