0512d8176964a0a64fc4ce0eb0761ae6ffb01df0
mutlu <mutlu.sancaktutar@hmcts.net>
2020-08-04 13:14:33 +0100

    add support for setting static final fields

---
+++ src/test/java/uk/gov/hmcts/common/TestUtils.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.common;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class TestUtils {
+
+    public static void setFinalStatic(Field field, Object newValue) throws Exception {
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+        field.set(null, newValue);
+    }
+
+}
