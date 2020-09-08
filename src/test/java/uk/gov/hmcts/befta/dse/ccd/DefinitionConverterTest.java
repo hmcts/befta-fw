0897edf4bc76d5cf7054ac56f12d9797f436f8fe
mutlu <mutlu.sancaktutar@hmcts.net>
2020-09-08 20:46:44 +0100

    use different temp folder every time

--- src/test/java/uk/gov/hmcts/befta/dse/ccd/DefinitionConverterTest.java
+++ src/test/java/uk/gov/hmcts/befta/dse/ccd/DefinitionConverterTest.java
@@ -19,11 +19,12 @@ import uk.gov.hmcts.befta.dse.ccd.definition.converter.FileUtils;
  */
 class DefinitionConverterTest {
 
-    public static final String DEFAULT_DEFINITIONS_PATH_JSON = "src/main/resources/uk/gov/hmcts/befta/dse/ccd/definitions/valid/CCD_CNP_27";
-	public static final String DEFAULT_DEFINITIONS_PATH_EXCEL = "src/main/resources/uk/gov/hmcts/befta/dse/ccd/definitions/excel/CCD_CNP_27.xlsx";
-    public static final String TEMPORARY_DEFINITION_FOLDER_JSON = "src/main/resources/uk/gov/hmcts/befta/dse/ccd/definition_files/json";
-    public static final String TEMPORARY_DEFINITION_FOLDER_EXCEL = "src/main/resources/uk/gov/hmcts/befta/dse/ccd/definition_files/excel";
-    public static final String TEMPORARY_DEFINITION_FOLDER = "src/main/resources/uk/gov/hmcts/befta/dse/ccd/definition_files";
+    private static final String DEFAULT_DEFINITIONS_PATH_JSON = "src/main/resources/uk/gov/hmcts/befta/dse/ccd/definitions/valid/CCD_CNP_27";
+    private static final String DEFAULT_DEFINITIONS_PATH_EXCEL = "src/main/resources/uk/gov/hmcts/befta/dse/ccd/definitions/excel/CCD_CNP_27.xlsx";
+    private static final String TEMPORARY_DEFINITION_FOLDER = "temp_dir/" + System.currentTimeMillis()
+            + "/definition_files";
+    public static final String TEMPORARY_DEFINITION_FOLDER_JSON = TEMPORARY_DEFINITION_FOLDER + "/json";
+    public static final String TEMPORARY_DEFINITION_FOLDER_EXCEL = TEMPORARY_DEFINITION_FOLDER + "/excel";
 
 	/**
 	 * Test method for {@link uk.gov.hmcts.befta.dse.ccd.DefinitionConverter#main(java.lang.String[])}.
