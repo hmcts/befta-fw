013f378b8b16ce8ee7d49c73eec9e459166f395b
Nitish Singla <nitish.singla@hmcts.net>
2020-09-21 10:29:23 +0100

    Am-992: Added logic to check for multiple flag names.

--- src/main/java/uk/gov/hmcts/befta/launchdarkly/LaunchDarklyFeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/befta/launchdarkly/LaunchDarklyFeatureToggleService.java
@@ -8,10 +8,11 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.befta.featureToggle.FeatureToggle;
-import uk.gov.hmcts.befta.player.DefaultBackEndFunctionalTestScenarioPlayer;
 import uk.gov.hmcts.befta.util.BeftaUtils;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class LaunchDarklyFeatureToggleService implements FeatureToggle {
@@ -31,14 +32,15 @@ public class LaunchDarklyFeatureToggleService implements FeatureToggle {
     @Override
     public void evaluateFlag(Scenario scenario) {
 
+        boolean isLDFlagEnabled = true;
         logger.info("Inside evaluateFlag");
         scenario.log("Inside evaluateFlag");
-        Optional<String> flagName = scenario.getSourceTagNames().stream()
+        List<String> flagNames = scenario.getSourceTagNames().stream()
                 .filter(tag -> tag.contains(LAUNCH_DARKLY_FLAG))
                 .map(tag -> tag.substring(tag.indexOf("(") + 1, tag.indexOf(")")))
-                .findFirst();
+                .collect(Collectors.toList());
 
-        if (ldClient != null && flagName.isPresent()) {
+        if (ldClient != null && !flagNames.isEmpty()) {
             if (LaunchDarklyConfig.getLDMicroserviceName() == null) {
                 BeftaUtils.skipScenario(scenario, ("The Scenario is being skipped as MICROSERVICE_NAME variable is not configured"));
 
@@ -53,17 +55,22 @@ public class LaunchDarklyFeatureToggleService implements FeatureToggle {
                     .custom(SERVICENAME, LaunchDarklyConfig.getLDMicroserviceName())
                     .build();
 
-            boolean isLDFlagEnabled = ldClient.boolVariation(flagName.get(), user, false);
+            for (String flag : flagNames) {
+                isLDFlagEnabled = ldClient.boolVariation(flag, user, false);
 
-            if (!isLDFlagEnabled) {
-                Optional<String> scenarioName = scenario.getSourceTagNames().stream()
-                        .filter(tag -> tag.contains("@S-"))
-                        .map(tag -> tag.substring(1))
-                        .findFirst();
+                if (!isLDFlagEnabled) {
+                    Optional<String> scenarioName = scenario.getSourceTagNames().stream()
+                            .filter(tag -> tag.contains("@S-"))
+                            .map(tag -> tag.substring(1))
+                            .findFirst();
 
-                BeftaUtils.skipScenario(scenario, String.format("The Scenario %s is being skipped as LD flag is disabled",
-                        scenarioName.orElse(StringUtils.EMPTY)));
+                    BeftaUtils.skipScenario(scenario, String.format("The Scenario %s is being skipped as LD flag %s is disabled",
+                            scenarioName.orElse(StringUtils.EMPTY), flag));
+                    break;
+                }
+
             }
+
         }
     }
 }
