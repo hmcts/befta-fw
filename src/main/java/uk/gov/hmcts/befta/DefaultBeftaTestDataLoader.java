91331a3ed04639ad1a77a38417be2c786122ebe3
VamshiRudrabhatlaHMCTS <vamshi.rudrabhatla@HMCTS.net>
2020-11-20 12:53:29 +0000

    Changes as per comments - 8934-Adding sysOuts for debug

--- src/main/java/uk/gov/hmcts/befta/DefaultBeftaTestDataLoader.java
+++ src/main/java/uk/gov/hmcts/befta/DefaultBeftaTestDataLoader.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.befta;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -38,17 +39,27 @@ public class DefaultBeftaTestDataLoader implements BeftaTestDataLoader {
 
     private boolean shouldSkipDataLoad() {
         try {
+            //declaring with a dummy last execution time
+            String recentExecutionTime  = "2020-01-01T00:00:00.001";
+            File recentExecutionFile = new File(TestAutomationAdapter.EXECUTION_INFO_FILE);
             double testDataLoadSkipPeriod = BeftaMain.getConfig().getTestDataLoadSkipPeriod();
-            RecentExecutionsInfo recentExecutionsInfo = JsonUtils
-                    .readObjectFromJsonFile(TestAutomationAdapter.EXECUTION_INFO_FILE, RecentExecutionsInfo.class);
-            String recentExecutionTime = recentExecutionsInfo.getLastExecutionTime();
-
-            if (isWithinSkipPeriod(
-                    recentExecutionTime,
-                    testDataLoadSkipPeriod)
-                    && wasMostRecentDataForSameTests(recentExecutionsInfo)) {
-                return true;
+            System.out.println("testDataLoadSkipPeriod from the config" + testDataLoadSkipPeriod);
+            if(recentExecutionFile.exists()) {
+                System.out.println("recent exec file exists");
+                RecentExecutionsInfo recentExecutionsInfo = JsonUtils
+                        .readObjectFromJsonFile(TestAutomationAdapter.EXECUTION_INFO_FILE, RecentExecutionsInfo.class);
+                recentExecutionTime = recentExecutionsInfo.getLastExecutionTime();
+                System.out.println("recent exec file exec time" + recentExecutionTime);
+                if (isWithinSkipPeriod(
+                        recentExecutionTime,
+                        testDataLoadSkipPeriod)
+                        && wasMostRecentDataForSameTests(recentExecutionsInfo)) {
+                    System.out.println("Should  skip loading data.");
+                    return true;
+                }
             }
+
+
         } catch (Exception e) {
             BeftaUtils.defaultLog("Should NOT skip loading data.", e);
             return false;
@@ -113,6 +124,7 @@ public class DefaultBeftaTestDataLoader implements BeftaTestDataLoader {
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
         LocalDateTime givenDateTIme = LocalDateTime.parse(recentExecutionTime, format);
         Long timeDifference = Duration.between(givenDateTIme, currentTime).toMinutes();
+        System.out.println("the values for time diff and skip period are and less is true- " + timeDifference + "," + testDataLoadSkipPeriod);
         return timeDifference < testDataLoadSkipPeriod;
     }
 
@@ -121,6 +133,10 @@ public class DefaultBeftaTestDataLoader implements BeftaTestDataLoader {
         String branchName = recentExecutionsInfo.getLastExecutionProjectBranch();
         String recentRepoSubString = repoName.substring(repoName.lastIndexOf("/")+1,repoName.length() );
         if (getCurrentGitRepo().contains(recentRepoSubString) && getCurrentGitBranch().equalsIgnoreCase(branchName)) {
+            System.out.println ("the repo and the branch from the recent execution matched" +  recentRepoSubString + "--"
+                    + getCurrentGitRepo()
+                    + "--"
+                    + branchName + "--" + getCurrentGitBranch());
             return true;
         } else {
             BeftaUtils.defaultLog("repo branch not matching -" + recentRepoSubString + "--"
