27aa3fd9c227adeecc681c989d4870ae7a37c24f
Gareth Sullivan <gareth.sullivan@cgi.com>
2020-12-01 14:19:02 +0000

    ACA-112 - Implementation updated following design rethink

--- src/main/java/uk/gov/hmcts/befta/dse/ccd/definition/converter/EnvironmentURLUtils.java
+++ src/main/java/uk/gov/hmcts/befta/dse/ccd/definition/converter/EnvironmentURLUtils.java
@@ -3,46 +3,69 @@ package uk.gov.hmcts.befta.dse.ccd.definition.converter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import uk.gov.hmcts.befta.BeftaMain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EnvironmentURLUtils {
 
-    private static final List<String> SHEETS_FOR_URL_SUBSTITUTIONS = Arrays.asList("CaseEvent");
-    private static final String LOCALHOST = "localhost";
-    private static final String PREVIEW = "service.core-compute-preview.internal";
-    private static final String DEFAULT_AAT_CALLBACK_HOST =
-            "ccd-test-stubs-service-aat.service.core-compute-aat.internal";
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String BASE_URL_PLACEHOLDER_REGEX = "\\$\\{(.*?)\\}";
+    private static final List<String> SHEETS_FOR_URL_SUBSTITUTIONS = Arrays.asList("CaseEvent", "CaseEventToFields");
 
-    public static JsonNode updateCallBackURLs(JsonNode rootSheetArray, String jsonFileName) throws JsonProcessingException {
-        return objectMapper.readTree(substituteWithEnvironmentSpecificURL(rootSheetArray.toString(), jsonFileName));
-    }
+    protected static final String MCA_API_BASE_URL = "MCA_API_BASE_URL";
+    protected static final String TEST_STUB_SERVICE_BASE_URL = "TEST_STUB_SERVICE_BASE_URL";
 
-    private static String substituteWithEnvironmentSpecificURL(String rootSheetArrayString, String jsonFileName) {
-        String testUrlString = BeftaMain.getConfig().getTestUrl();
-        String returnValue = rootSheetArrayString;
-
-        if (testUrlString != null && SHEETS_FOR_URL_SUBSTITUTIONS.contains(jsonFileName)) {
-            if (testUrlString.contains(LOCALHOST)) {
-                returnValue =
-                        rootSheetArrayString.replaceAll(DEFAULT_AAT_CALLBACK_HOST, "ccd-test-stubs-service:5555");
-            }
-
-            if (testUrlString.endsWith(PREVIEW)) {
-                try {
-                    URL testURL = new URL(testUrlString);
-                    returnValue = rootSheetArrayString.replaceAll(DEFAULT_AAT_CALLBACK_HOST, testURL.getHost());
-                } catch (MalformedURLException e) {
-                    returnValue = rootSheetArrayString;
-                }
-            }
+    public static JsonNode updateCallBackURLs(JsonNode rootSheetArray, String jsonFileName)
+            throws JsonProcessingException, MalformedURLException {
+        if (SHEETS_FOR_URL_SUBSTITUTIONS.contains(jsonFileName)) {
+            return new ObjectMapper().readTree(parseCallbackHostValues(rootSheetArray.toString()));
+        } else {
+            return rootSheetArray;
         }
-        return returnValue;
     }
 
+    private static String parseCallbackHostValues(String sheet) throws MalformedURLException {
+        Matcher matcher = Pattern.compile(BASE_URL_PLACEHOLDER_REGEX).matcher(sheet);
+
+        while (matcher.find()) {
+            BaseUrlPlaceholder matchedResult = parseDefaultHostValue(matcher.group(1));
+
+            String replacementUrl = getURLStringFromEnvironmentValue(matchedResult.environmentVariable);
+            if (replacementUrl == null) {
+                replacementUrl = matchedResult.urlDefaultValue;
+            }
+            sheet = sheet.replaceAll(String.format("\\$\\{%s.*?\\}", matchedResult.environmentVariable),
+                    replacementUrl);
+        }
+
+        return sheet;
+    }
+
+    private static String getURLStringFromEnvironmentValue(String environmentName) throws MalformedURLException {
+        String environmentValue = System.getenv(environmentName);
+        return environmentValue != null ? new URL(environmentValue).toString() : null;
+    }
+
+    private static BaseUrlPlaceholder parseDefaultHostValue(String value) {
+        BaseUrlPlaceholder baseUrlPlaceholder = null;
+        if (value != null) {
+            String[] split = value.split(":", 2);
+            baseUrlPlaceholder = new BaseUrlPlaceholder(split[0], split[1]);
+        }
+
+        return baseUrlPlaceholder;
+    }
+
+    @AllArgsConstructor
+    @Data
+    private static class BaseUrlPlaceholder {
+        private String environmentVariable;
+        private String urlDefaultValue;
+    }
 }
--- src/test/java/uk/gov/hmcts/befta/dse/ccd/definition/converter/EnvironmentURLUtilsTest.java
+++ src/test/java/uk/gov/hmcts/befta/dse/ccd/definition/converter/EnvironmentURLUtilsTest.java
@@ -7,72 +7,237 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EnvironmentURLUtilsTest {
 
     private static final String CASE_EVENT =  "CaseEvent";
-    private static final String TEST_CASE_EVENT_FILE_LOCATION = "framework-test-data/environment-url-utils-test-data/CaseEvent.json";
-    private static final String PR_HOST = "aac-manage-case-assignment-pr-164.service.core-compute-preview.internal";
-    private static final String LOCALHOST_URL = "http://localhost:8080/dummy-api";
-    private static final String TEST_URL_ENV = "TEST_URL";
+    private static final String LOCALHOST_URL = "http://localhost:8080";
 
     ObjectMapper objectMapper = new ObjectMapper();
 
-    private File caseEventFile;
     JsonNode caseEventJson;
+
+    private String CASE_EVENT_TO_FIELDS_TEMPLATE = "[ {\n" +
+            "  \"CaseTypeID\" : \"FT_MultiplePages\",\n" +
+            "  \"CaseEventID\" : \"createCase\",\n" +
+            "  \"CaseFieldID\" : \"TextFieldFName\",\n" +
+            "  \"PageID\" : \"MuliFormPage1\",\n" +
+            "  \"PageLabel\" : \"Case Multiple Pages: Page 1/3\",\n" +
+            "  \"CallBackURLMidEvent\" : \"%s%s\"\n" +
+            "}, {\n" +
+            " " +
+            "  \"CaseTypeID\" : \"FT_MultiplePages\",\n" +
+            "  \"CaseEventID\" : \"createCase\",\n" +
+            "  \"CaseFieldID\" : \"TextFieldMName\",\n" +
+            "  \"PageID\" : \"MuliFormPage2\",\n" +
+            "  \"PageLabel\" : \"Case Multiple Pages: Page 2/3\",\n" +
+            "  \"CallBackURLMidEvent\" : \"\"\n" +
+            "} ]\n";
+
+    private String CASE_EVENT_CONTENT_TEMPLATE = "[ {\n" +
+            "  \"CaseTypeID\" : \"FT_NoCAutoApprovalCaseType\",\n" +
+            "  \"ID\" : \"createCase\",\n" +
+            "  \"Name\" : \"Create Case\",\n" +
+            "  \"Description\" : \"\",\n" +
+            "  \"CallBackURLAboutToStartEvent\" : \"\",\n" +
+            "  \"RetriesTimeoutAboutToStartEvent\" : \"\",\n" +
+            "  \"CallBackURLAboutToSubmitEvent\" : \"\",\n" +
+            "  \"RetriesTimeoutURLAboutToSubmitEvent\" : \"\",\n" +
+            "  \"CallBackURLSubmittedEvent\" : \"\",\n" +
+            "  \"RetriesTimeoutURLSubmittedEvent\" : \"\",\n" +
+            "  \"SecurityClassification\" : \"Public\"\n" +
+            "}, {\n" +
+            "  \"CaseTypeID\" : \"FT_NoCAutoApprovalCaseType\",\n" +
+            "  \"ID\" : \"nocRequest_autoApproval\",\n" +
+            "  \"Name\" : \"NoC Request\",\n" +
+            "  \"Description\" : \"Notice of Change Request\",\n" +
+            "  \"CallBackURLAboutToStartEvent\" : \"\",\n" +
+            "  \"RetriesTimeoutAboutToStartEvent\" : \"\",\n" +
+            "  \"CallBackURLAboutToSubmitEvent\" : \"\",\n" +
+            "  \"RetriesTimeoutURLAboutToSubmitEvent\" : \"\",\n" +
+            "  \"CallBackURLSubmittedEvent\" : \"%s%s\",\n" +
+            "  \"RetriesTimeoutURLSubmittedEvent\" : \"\",\n" +
+            "  \"SecurityClassification\" : \"Public\"\n" +
+            "}, {\n" +
+            "  \"CaseTypeID\" : \"FT_NoCAutoApprovalCaseType\",\n" +
+            "  \"ID\" : \"ApplyNoCDecision\",\n" +
+            "  \"Name\" : \"Apply NoC Decision\",\n" +
+            "  \"Description\" : \"Apply Notice of Change Request\",\n" +
+            "  \"CallBackURLAboutToStartEvent\" : \"%s%s\",\n" +
+            "  \"RetriesTimeoutAboutToStartEvent\" : \"\",\n" +
+            "  \"CallBackURLAboutToSubmitEvent\" : \"\",\n" +
+            "  \"RetriesTimeoutURLAboutToSubmitEvent\" : \"\",\n" +
+            "  \"CallBackURLSubmittedEvent\" : \"\",\n" +
+            "  \"RetriesTimeoutURLSubmittedEvent\" : \"\",\n" +
+            "  \"SecurityClassification\" : \"Public\"\n" +
+            "} ]";
+
+    private static final String NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_DEFAULT_HOST = "http://localhost:4454";
+    private static final String NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_HOST =
+            String.format("${MCA_API_BASE_URL:%s}", NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_DEFAULT_HOST);
+    private static final String NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_PATH = "/noc/check-noc-approval";
+
+    private static final String APPLY_NOC_DECISION_SUBMIT_EVENT_DEFAULT_HOST = "http://localhost:5555";
+    private static final String APPLY_NOC_DECISION_SUBMIT_EVENT_HOST =
+            String.format("${TEST_STUB_SERVICE_BASE_URL:%s}", APPLY_NOC_DECISION_SUBMIT_EVENT_DEFAULT_HOST);
+    private static final String APPLY_NOC_DECISION_SUBMIT_EVENT_PATH =
+            "/callback_returning_simulated_completed_noc_request";
+
+    private static final String CREATE_CASE_CALLBACK_MID_EVENT_DEFAULT_HOST
+            = "http://ccd-test-stubs-service:5555";
+    private static final String CREATE_CASE_CALLBACK_MID_EVENT_HOST
+            = String.format("${TEST_STUB_SERVICE_BASE_URL:%s}", CREATE_CASE_CALLBACK_MID_EVENT_DEFAULT_HOST);
+    private static final String CREATE_CASE_CALLBACK_MID_EVENT_PATH
+            = "/case_type/fe-functional-test/mid_event_dynamic_list";
 
     @BeforeEach
     void setup() throws IOException {
-        caseEventFile = new File(ClassLoader.getSystemResource(TEST_CASE_EVENT_FILE_LOCATION).getFile());
-        caseEventJson = objectMapper.readTree(caseEventFile);
+        caseEventJson = objectMapper.readTree(
+                String.format(CASE_EVENT_CONTENT_TEMPLATE,
+                        NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_HOST, NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_PATH,
+                        APPLY_NOC_DECISION_SUBMIT_EVENT_HOST, APPLY_NOC_DECISION_SUBMIT_EVENT_PATH)
+        );
     }
 
     @Test
-    @SetEnvironmentVariable(key = TEST_URL_ENV, value = LOCALHOST_URL)
-    void onlyModifyCaseEventFiles() throws JsonProcessingException {
-        JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventJson, "AFileNameThatIsNotCaseEvent");
-        assertEquals(caseEventJson, modifiedJsonNode);
+    void unsupportedSheetNamesShouldNotModifyURLs() throws JsonProcessingException, MalformedURLException {
+        JsonNode nullJsonNode = objectMapper.nullNode();
+
+        JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(nullJsonNode, "AFileNameThatIsNotCaseEvent");
+        assertEquals(nullJsonNode, modifiedJsonNode);
     }
 
     @Test
-    @SetEnvironmentVariable(key = TEST_URL_ENV, value = "http://localhost:8080/dummy-api")
-    void urlsAreConvertedToLocalHostOnLocalEnvironment() throws IOException {
+    @SetEnvironmentVariable(key = EnvironmentURLUtils.TEST_STUB_SERVICE_BASE_URL, value = LOCALHOST_URL)
+    void caseEventToFieldsSheetModifyTestStubURL() throws JsonProcessingException, MalformedURLException {
+        JsonNode caseEventToFieldsNode = objectMapper.readTree(String.format(CASE_EVENT_TO_FIELDS_TEMPLATE,
+                CREATE_CASE_CALLBACK_MID_EVENT_HOST,
+                CREATE_CASE_CALLBACK_MID_EVENT_PATH));
+
+        JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventToFieldsNode, "CaseEventToFields");
+
+        // check JSON has been modified
+        assertNotEquals(caseEventToFieldsNode, modifiedJsonNode);
+
+        // check placeholders are not still present
+        assertFalse(modifiedJsonNode.toString().contains(CREATE_CASE_CALLBACK_MID_EVENT_HOST));
+
+        // check URLs have been substituted with correct values
+        assertTrue(modifiedJsonNode.toString().contains(LOCALHOST_URL+ CREATE_CASE_CALLBACK_MID_EVENT_PATH));
+    }
+
+    @Test
+    void caseEventToFieldsUpdatedWithDefaultValuesWheNoEnvironmentVariablesSet()
+            throws JsonProcessingException, MalformedURLException {
+        JsonNode caseEventToFieldsNode = objectMapper.readTree(String.format(CASE_EVENT_TO_FIELDS_TEMPLATE,
+                CREATE_CASE_CALLBACK_MID_EVENT_HOST,
+                CREATE_CASE_CALLBACK_MID_EVENT_PATH));
+
+        JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventToFieldsNode, "CaseEventToFields");
+
+        // check JSON has been modified
+        assertNotEquals(caseEventToFieldsNode, modifiedJsonNode);
+
+        // check placeholders are not still present
+        assertFalse(modifiedJsonNode.toString().contains(CREATE_CASE_CALLBACK_MID_EVENT_HOST));
+
+        // check URLs have been substituted with correct values
+        assertTrue(modifiedJsonNode.toString()
+                .contains(CREATE_CASE_CALLBACK_MID_EVENT_DEFAULT_HOST + CREATE_CASE_CALLBACK_MID_EVENT_PATH));
+    }
+
+    @Test
+    void caseEventUrlsUpdatedWithDefaultValuesWheNoEnvironmentVariablesSet() throws IOException {
         JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventJson, CASE_EVENT);
+
+        // check JSON has been modified
         assertNotEquals(caseEventJson, modifiedJsonNode);
-        assertFalse(modifiedJsonNode.toString().contains("aat"));
-        assertTrue(modifiedJsonNode.toString().contains("ccd-test-stubs-service:5555"));
+
+        // check placeholders are not still present
+        assertFalse(modifiedJsonNode.toString().contains(NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_HOST));
+        assertFalse(modifiedJsonNode.toString().contains(APPLY_NOC_DECISION_SUBMIT_EVENT_HOST));
+
+        // check URLs have been substituted with correct values
+        assertTrue(modifiedJsonNode.toString()
+                .contains(NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_DEFAULT_HOST +
+                          NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_PATH));
+        assertTrue(modifiedJsonNode.toString()
+                .contains(APPLY_NOC_DECISION_SUBMIT_EVENT_DEFAULT_HOST +
+                          APPLY_NOC_DECISION_SUBMIT_EVENT_PATH));
     }
 
     @Test
-    @SetEnvironmentVariable(key = TEST_URL_ENV, value = "http://my.aat.environment:8080/dummy-api")
-    void urlsAreNotModifiedWhenRunningOnAatEnvironment() throws IOException {
+    @SetEnvironmentVariable(key = EnvironmentURLUtils.MCA_API_BASE_URL, value = LOCALHOST_URL)
+    void caseEventMcaApiBaseUrlUpdated() throws IOException {
         JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventJson, CASE_EVENT);
-        assertEquals(caseEventJson, modifiedJsonNode);
-        assertTrue(modifiedJsonNode.toString().contains("aat"));
-        assertFalse(modifiedJsonNode.toString().contains("ccd-test-stubs-service:5555"));
-    }
 
-    @Test
-    @SetEnvironmentVariable(key = TEST_URL_ENV, value = "https://" + PR_HOST)
-    void urlsConvertedToPRHostOnPreviewEnvironment() throws IOException {
-        JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventJson, CASE_EVENT);
+        // check JSON has been modified
         assertNotEquals(caseEventJson, modifiedJsonNode);
-        assertFalse(modifiedJsonNode.toString().contains("aat"));
-        assertTrue(modifiedJsonNode.toString().contains("https://" + PR_HOST));
-        assertTrue(modifiedJsonNode.toString().contains("http://" + PR_HOST));
+
+        // check placeholders are not still present
+        assertFalse(modifiedJsonNode.toString().contains(NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_HOST));
+        assertFalse(modifiedJsonNode.toString().contains(APPLY_NOC_DECISION_SUBMIT_EVENT_HOST));
+
+        // check URLs have been substituted with correct values
+        assertTrue(modifiedJsonNode.toString().contains(LOCALHOST_URL + NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_PATH));
+        assertFalse(modifiedJsonNode.toString().contains(LOCALHOST_URL + APPLY_NOC_DECISION_SUBMIT_EVENT_PATH));
+        assertTrue(modifiedJsonNode.toString()
+                .contains(APPLY_NOC_DECISION_SUBMIT_EVENT_DEFAULT_HOST + APPLY_NOC_DECISION_SUBMIT_EVENT_PATH));
     }
 
     @Test
-    @SetEnvironmentVariable(key = TEST_URL_ENV, value = "httpsMalformed://" + PR_HOST)
-    void urlNotConvertedOnPreviewEnvironmentTestUrlMalformed() throws IOException {
+    @SetEnvironmentVariable(key = EnvironmentURLUtils.TEST_STUB_SERVICE_BASE_URL, value = LOCALHOST_URL)
+    void caseEventTestStubServiceBaseUrlUpdated() throws IOException {
         JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventJson, CASE_EVENT);
-        assertEquals(caseEventJson, modifiedJsonNode);
+
+        // check JSON has been modified
+        assertNotEquals(caseEventJson, modifiedJsonNode);
+
+        // check placeholders are not still present
+        assertFalse(modifiedJsonNode.toString().contains(NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_HOST));
+        assertFalse(modifiedJsonNode.toString().contains(APPLY_NOC_DECISION_SUBMIT_EVENT_HOST));
+
+        // check URLs have been substituted with correct values
+        assertTrue(modifiedJsonNode.toString()
+                .contains(NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_DEFAULT_HOST +
+                          NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_PATH));
+        assertFalse(modifiedJsonNode.toString().contains(LOCALHOST_URL + NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_PATH));
+        assertTrue(modifiedJsonNode.toString().contains(LOCALHOST_URL + APPLY_NOC_DECISION_SUBMIT_EVENT_PATH));
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = EnvironmentURLUtils.MCA_API_BASE_URL, value = LOCALHOST_URL)
+    @SetEnvironmentVariable(key = EnvironmentURLUtils.TEST_STUB_SERVICE_BASE_URL, value = LOCALHOST_URL)
+    void caseEventAllBaseUrlsUpdated() throws IOException {
+        JsonNode modifiedJsonNode = EnvironmentURLUtils.updateCallBackURLs(caseEventJson, CASE_EVENT);
+
+        // check JSON has been modified
+        assertNotEquals(caseEventJson, modifiedJsonNode);
+
+        // check placeholders are not still present
+        assertFalse(modifiedJsonNode.toString().contains(NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_HOST));
+        assertFalse(modifiedJsonNode.toString().contains(APPLY_NOC_DECISION_SUBMIT_EVENT_HOST));
+
+        // check URLs have been substituted with correct values
+        assertTrue(modifiedJsonNode.toString().contains(LOCALHOST_URL + NOC_REQUEST_AUTO_APPROVAL_SUBMIT_EVENT_PATH));
+        assertTrue(modifiedJsonNode.toString().contains(LOCALHOST_URL + APPLY_NOC_DECISION_SUBMIT_EVENT_PATH));
+    }
+
+    @Test
+    @SetEnvironmentVariable(key = EnvironmentURLUtils.MCA_API_BASE_URL, value = "httpsMalformed://" + LOCALHOST_URL)
+    void urlNotConvertedOnPreviewEnvironmentTestUrlMalformed()  {
+        Exception exception = assertThrows(MalformedURLException.class,
+                () -> EnvironmentURLUtils.updateCallBackURLs(caseEventJson, CASE_EVENT));
+        assertNotNull(exception);
+        assertTrue(exception.getMessage().contains("unknown protocol"));
     }
 }
