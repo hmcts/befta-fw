423b4677400d022e54054d6a58d76abc618eea76
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-02-02 10:13:17 +0000

    RDM-9765 - Add env var for test failing on pipeline

--- src/test/java/uk/gov/hmcts/befta/DefaultTestAutomationAdapterTest.java
+++ src/test/java/uk/gov/hmcts/befta/DefaultTestAutomationAdapterTest.java
@@ -210,6 +210,7 @@ class DefaultTestAutomationAdapterTest {
     @Test
     @SetEnvironmentVariable(key = "BEFTA_OAUTH2_REDIRECT_URI_OF_OTHER", value = "BEFTA_OAUTH2_REDIRECT_URI_OF_OTHER_VALUE")
     @SetEnvironmentVariable(key = "BEFTA_OAUTH2_CLIENT_SECRET_OF_OTHER", value = "BEFTA_OAUTH2_CLIENT_SECRET_OF_OTHER_VALUE")
+    @SetEnvironmentVariable(key = BEFTA_S2S_CLIENT_ID_KEY, value = BEFTA_S2S_CLIENT_ID_VALUE)
     void testAuthenticateTimeToLiveNotSet() throws Exception {
         UserData user = new UserData("user", "pwd");
         user.setId("id");
