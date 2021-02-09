03e67bda28a729ae2b929a90ecd09c629292182c
Gareth Sullivan <gareth.sullivan@cgi.com>
2021-02-09 16:58:25 +0000

    RDM-10737 - moved getVerificationConfigFrom to allow thorough testing

--- src/main/java/uk/gov/hmcts/befta/data/CollectionVerificationConfig.java
+++ src/main/java/uk/gov/hmcts/befta/data/CollectionVerificationConfig.java
@@ -3,6 +3,11 @@ package uk.gov.hmcts.befta.data;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+import uk.gov.hmcts.befta.BeftaMain;
+import uk.gov.hmcts.befta.util.ElementIdFinder;
+
+import java.util.Collection;
+import java.util.Map;
 
 @Data
 public class CollectionVerificationConfig {
@@ -51,4 +56,42 @@ public class CollectionVerificationConfig {
         return this == DEFAULT;
     }
 
+    public static CollectionVerificationConfig getVerificationConfigFrom(Collection<?> collection) {
+        if (collection == null || collection.isEmpty()) {
+            return DEFAULT;
+        }
+        Object firstElement = collection.iterator().next();
+        if (firstElement instanceof CollectionVerificationConfig) {
+            return (CollectionVerificationConfig) firstElement;
+        }
+        if (firstElement instanceof Map<?, ?>) {
+            Map<?, ?> firstMap = (Map<?, ?>) firstElement;
+            if (firstMap.containsKey(OPERATOR_FIELD_NAME) || firstMap.containsKey(ORDERING_FIELD_NAME)
+                    || firstMap.containsKey(ELEMENT_ID_FIELD_NAME)) {
+                CollectionVerificationConfig config = new CollectionVerificationConfig();
+                Operator operator = Operator.of((String) firstMap.get(OPERATOR_FIELD_NAME));
+                if (operator != null) {
+                    config.setOperator(operator);
+                }
+                Ordering ordering = Ordering.of((String) firstMap.get(ORDERING_FIELD_NAME));
+                if (ordering != null) {
+                    config.setOrdering(ordering);
+                } else {
+                    config.setOrdering(BeftaMain.getConfig().getDefaultCollectionAssertionMode());
+                }
+
+                if (config.getOrdering() == Ordering.UNORDERED) {
+                    String idString = (String) firstMap.get(ELEMENT_ID_FIELD_NAME);
+
+                    if (idString != null) {
+                        config.setElementId(idString);
+                    } else {
+                        config.setElementId(ElementIdFinder.findElementIds(collection));
+                    }
+                }
+                return config;
+            }
+        }
+        return DEFAULT;
+    }
 }
--- src/test/java/uk/gov/hmcts/befta/data/CollectionVerificationConfigTest.java
+++ src/test/java/uk/gov/hmcts/befta/data/CollectionVerificationConfigTest.java
@@ -3,18 +3,37 @@
  */
 package uk.gov.hmcts.befta.data;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
 import uk.gov.hmcts.befta.data.CollectionVerificationConfig.Operator;
 import uk.gov.hmcts.befta.data.CollectionVerificationConfig.Ordering;
+import uk.gov.hmcts.befta.exception.InvalidTestDataException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.unmodifiableCollection;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author korneleehenry
  *
  */
 class CollectionVerificationConfigTest {
+
+	private static final String CASE_ID = "case_id";
+	private static final String CASE_ID_VALUE = "case_id_value";
+	private static final String USER_ID = "user_id";
+	private static final String CASE_ROLE_KEY = "case_role_key";
+	private static final String USER_ID_VALUE = "user_id_value";
+	private static final String CASE_ROLE_VALUE = "case_role_value";
 
 	/**
 	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#equals(java.lang.Object)}.
@@ -40,4 +59,170 @@ class CollectionVerificationConfigTest {
 		assertNotNull(other.toString());
 	}
 
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigFromNullCollection() {
+		assertEquals(CollectionVerificationConfig.DEFAULT, CollectionVerificationConfig.getVerificationConfigFrom(null));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigFromEmptyCollection() {
+		assertEquals(CollectionVerificationConfig.DEFAULT,
+				CollectionVerificationConfig.getVerificationConfigFrom(unmodifiableCollection(Collections.emptyList())));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigReturnsDefaultFromListCollectionWhereirstElementIsCollectionVerificationConfig() {
+		assertEquals(CollectionVerificationConfig.DEFAULT,
+				CollectionVerificationConfig.getVerificationConfigFrom(unmodifiableCollection(asList(CollectionVerificationConfig.DEFAULT))));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigReturnsDefaultFromListCollectionWhereFirstElementIsNotCollectionVerificationConfig() {
+
+		assertEquals(CollectionVerificationConfig.DEFAULT,
+				CollectionVerificationConfig.getVerificationConfigFrom((Collection<?>) Collections.EMPTY_LIST));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigReturnsDefaultFromMapNotContainingDoubleUnderscoredKeys() {
+
+		List<Map<String, String>> listOfMaps = new ArrayList<>();
+		listOfMaps.add(Collections.emptyMap());
+
+		assertEquals(CollectionVerificationConfig.DEFAULT,
+				CollectionVerificationConfig.getVerificationConfigFrom(listOfMaps));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigReturnsConfigFromMapContainingOperatorFieldName() {
+
+		Map<String, String> map = new HashMap<>();
+
+		map.put(CollectionVerificationConfig.OPERATOR_FIELD_NAME, Operator.SUBSET.name());
+
+		List<Map<String, String>> listOfMaps = new ArrayList<>();
+		listOfMaps.add(map);
+
+		CollectionVerificationConfig collectionVerificationConfig = new CollectionVerificationConfig(Operator.SUBSET, Ordering.ORDERED, "id");
+
+		assertEquals(collectionVerificationConfig,
+				CollectionVerificationConfig.getVerificationConfigFrom(listOfMaps));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigReturnsConfigFromMapContainingOperatorFieldNameAndOrderingFieldNameORDERED() {
+
+		Map<String, String> map = new HashMap<>();
+
+		map.put(CollectionVerificationConfig.OPERATOR_FIELD_NAME, Operator.SUBSET.name());
+		map.put(CollectionVerificationConfig.ORDERING_FIELD_NAME, Ordering.ORDERED.name());
+
+		List<Map<String, String>> listOfMaps = new ArrayList<>();
+		listOfMaps.add(map);
+
+		CollectionVerificationConfig collectionVerificationConfig = new CollectionVerificationConfig(Operator.SUBSET, Ordering.ORDERED, "id");
+
+		assertEquals(collectionVerificationConfig,
+				CollectionVerificationConfig.getVerificationConfigFrom(listOfMaps));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigReturnsConfigFromMapContainingOperatorFieldNameAndOrderingFieldNameUNORDERED() {
+
+		Map<String, String> map = new HashMap<>();
+
+		map.put(CollectionVerificationConfig.OPERATOR_FIELD_NAME, Operator.SUBSET.name());
+		map.put(CollectionVerificationConfig.ORDERING_FIELD_NAME, Ordering.UNORDERED.name());
+		map.put(CollectionVerificationConfig.ELEMENT_ID_FIELD_NAME, "idFieldName");
+		List<Map<String, String>> listOfMaps = new ArrayList<>();
+		listOfMaps.add(map);
+
+		CollectionVerificationConfig collectionVerificationConfig = new CollectionVerificationConfig(Operator.SUBSET, Ordering.UNORDERED, "idFieldName");
+
+		assertEquals(collectionVerificationConfig,
+				CollectionVerificationConfig.getVerificationConfigFrom(listOfMaps));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigReturnsConfigFromMapContainingOperatorFieldNameAndOrderingFieldNameUNORDEREDAutoCalculatedFieldNames() {
+
+		Map<String, String> metaDataMap = new HashMap<>();
+
+		metaDataMap.put(CollectionVerificationConfig.OPERATOR_FIELD_NAME, Operator.SUBSET.name());
+		metaDataMap.put(CollectionVerificationConfig.ORDERING_FIELD_NAME, Ordering.UNORDERED.name());
+
+		Map<String, String> dataElement1 = new HashMap<>();
+		dataElement1.put(CASE_ID, CASE_ID_VALUE);
+		dataElement1.put(USER_ID, USER_ID_VALUE);
+		dataElement1.put(CASE_ROLE_KEY, CASE_ROLE_VALUE);
+
+		Map<String, String> dataElement2 = new HashMap<>();
+		dataElement2.put(CASE_ID, CASE_ID_VALUE + "_1");
+		dataElement2.put(USER_ID, USER_ID + "_1");
+		dataElement2.put(CASE_ROLE_KEY, CASE_ROLE_VALUE + "_1");
+
+
+		List<Map<String, String>> listOfMaps = new ArrayList<>();
+		listOfMaps.add(metaDataMap);
+		listOfMaps.add(dataElement1);
+		listOfMaps.add(dataElement2);
+
+		CollectionVerificationConfig collectionVerificationConfig = new CollectionVerificationConfig(Operator.SUBSET, Ordering.UNORDERED, "case_id,case_role_key,user_id");
+
+		assertEquals(collectionVerificationConfig,
+				CollectionVerificationConfig.getVerificationConfigFrom(listOfMaps));
+	}
+
+	/**
+	 * Test method for {@link uk.gov.hmcts.befta.data.CollectionVerificationConfig#getVerificationConfigFrom(Collection)} ()}.
+	 */
+	@Test
+	void testVerificationConfigThrowsExceptionWhenCannotCalculateFieldNames() {
+
+		Map<String, String> metaDataMap = new HashMap<>();
+
+		metaDataMap.put(CollectionVerificationConfig.OPERATOR_FIELD_NAME, Operator.SUBSET.name());
+		metaDataMap.put(CollectionVerificationConfig.ORDERING_FIELD_NAME, Ordering.UNORDERED.name());
+
+		Map<String, String> dataElement1 = new HashMap<>();
+		dataElement1.put(CASE_ID, CASE_ID_VALUE);
+
+		Map<String, String> dataElement2 = new HashMap<>();
+		dataElement2.put(CASE_ID, CASE_ID_VALUE);
+
+		List<Map<String, String>> listOfMaps = new ArrayList<>();
+
+		listOfMaps.add(metaDataMap);
+		listOfMaps.add(dataElement1);
+		listOfMaps.add(dataElement2);
+
+		assertThrows(InvalidTestDataException.class, () -> CollectionVerificationConfig.getVerificationConfigFrom(listOfMaps));
+	}
 }
