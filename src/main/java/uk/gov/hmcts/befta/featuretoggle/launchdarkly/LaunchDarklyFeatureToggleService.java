58179b3beeed0f4baa0d9c1aa5e760997c962b7c
Nitish Singla <nitish.singla@hmcts.net>
2021-05-28 14:07:02 +0100

    AM-1385: Cleaned up the file.

--- src/main/java/uk/gov/hmcts/befta/featuretoggle/launchdarkly/LaunchDarklyFeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/befta/featuretoggle/launchdarkly/LaunchDarklyFeatureToggleService.java
@@ -2,13 +2,8 @@ package uk.gov.hmcts.befta.featuretoggle.launchdarkly;
 
 import com.launchdarkly.sdk.LDUser;
 import com.launchdarkly.sdk.server.LDClient;
-import io.cucumber.java.Scenario;
 import uk.gov.hmcts.befta.exception.FeatureToggleCheckFailureException;
 import uk.gov.hmcts.befta.featuretoggle.FeatureToggleService;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class LaunchDarklyFeatureToggleService implements FeatureToggleService<String, Boolean> {
 
@@ -24,7 +19,7 @@ public class LaunchDarklyFeatureToggleService implements FeatureToggleService<St
     private static final String LAUNCH_DARKLY_FLAG_WITH_EXPECTED_VALUE = "FeatureFlagWithExpectedValue";
     private static final String EXTERNAL_FLAG_WITH_EXPECTED_VALUE = "ExternalFlagWithExpectedValue";
 
-    LDClient ldClient = LaunchDarklyConfig.getLdInstance();
+    private final LDClient ldClient = LaunchDarklyConfig.getLdInstance();
 
     @Override
     public Boolean getToggleStatusFor(String flagId) {
@@ -45,33 +40,6 @@ public class LaunchDarklyFeatureToggleService implements FeatureToggleService<St
             throw new FeatureToggleCheckFailureException(
                     "The Scenario is being skipped as LAUNCH_DARKLY_ENV is not configured");
         }
-    }
-
- /*   private List<String> getFeatureFlagsOn(Scenario scenario) {
-        return scenario.getSourceTagNames().stream().filter(tag -> tag.contains(LAUNCH_DARKLY_FLAG))
-                .map(tag -> tag.substring(tag.indexOf("(") + 1, tag.indexOf(")"))).collect(Collectors.toList());
-    }*/
-
-    private Map<String, Boolean> getFeatureFlagsWithExpectedValue(Scenario scenario) {
-
-        return scenario.getSourceTagNames()
-                .stream()
-                .filter(tag -> tag.contains(LAUNCH_DARKLY_FLAG_WITH_EXPECTED_VALUE))
-                .map(tag -> tag.substring(tag.indexOf("(") + 1, tag.indexOf(")")))
-                .map(str -> str.split(","))
-                .collect(Collectors.toMap(str -> str[0], str -> Boolean.parseBoolean(str[1])));
-    }
-
-    private Map<String, Boolean> getExternalFlagsWithDefaultValue(Scenario scenario) {
-        Map<String, Boolean> externalFlagMap = new HashMap<>();
-        scenario.getSourceTagNames().forEach(tagname -> {
-            if (tagname.contains(EXTERNAL_FLAG_WITH_EXPECTED_VALUE)) {
-                String[] array = tagname.substring(tagname.indexOf("(") + 1, tagname.indexOf(")")).split(",");
-                externalFlagMap.put(array[0], Boolean.valueOf(array[1]));
-            }
-        });
-
-        return externalFlagMap;
     }
 
 }
