653cb6b3cd461c1ab0eb9492b2d04640aa46f1d6
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2026-09-14 16:22:36 +0100

    Make BEFTA_OAUTH2_SCOPE_VARIABLES_OF_X conditionally required on BEFTA_OAUTH2_ACCESS_TOKEN_TYPE_OF_X = OIDC

--- src/main/java/uk/gov/hmcts/befta/auth/UserTokenProviderConfig.java
+++ src/main/java/uk/gov/hmcts/befta/auth/UserTokenProviderConfig.java
@@ -35,9 +35,11 @@ public class UserTokenProviderConfig {
         clientId = tokenProviderClientId;
         clientSecret = EnvironmentVariableUtils.getRequiredVariable("BEFTA_OAUTH2_CLIENT_SECRET_OF_" + tokenProviderClientId.toUpperCase());
         redirectUri = EnvironmentVariableUtils.getRequiredVariable("BEFTA_OAUTH2_REDIRECT_URI_OF_" + tokenProviderClientId.toUpperCase());
-        scopeVariables = EnvironmentVariableUtils.getOptionalVariable("BEFTA_OAUTH2_SCOPE_VARIABLES_OF_" + tokenProviderClientId.toUpperCase());
-        accessTokenType = EnvironmentVariableUtils.getOptionalVariable(
-                "BEFTA_OAUTH2_ACCESS_TOKEN_TYPE_OF_" + tokenProviderClientId.toUpperCase());
+        String accessTokenTypeEnvVar = "BEFTA_OAUTH2_ACCESS_TOKEN_TYPE_OF_" + tokenProviderClientId.toUpperCase();
+        String scopeVariablesEnvVar = "BEFTA_OAUTH2_SCOPE_VARIABLES_OF_" + tokenProviderClientId.toUpperCase();
+        scopeVariables = EnvironmentVariableUtils.getConditionallyRequiredVariable(scopeVariablesEnvVar, accessTokenTypeEnvVar, OIDC);
+        
+        accessTokenType = EnvironmentVariableUtils.getOptionalVariable(accessTokenTypeEnvVar);
         if (accessTokenType == null) {
             accessTokenType = OAUTH2;
         }
